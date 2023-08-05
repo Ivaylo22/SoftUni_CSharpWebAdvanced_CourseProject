2a7b7c59d19d069bb342b6a7a94d201958cc0275
Ivaylo Petkov <ivi_petkow@abv.bg>
2023-08-05 16:56:36 +0300

    Added back buttons to all pages

---
+++ PizzaRestaurant.Web/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaRestaurant.Web.Controllers
+{
+    public class CartController : BaseController
+    {
+        [HttpPost]
+        public IActionResult AddToCart(int pizzaId, decimal updatedTotalPrice)
+        {
+
+            
+
+            // Redirect to a page, return a JSON response, or perform other necessary actions.
+            return RedirectToAction("CartContents"); // Redirect to the cart contents page.
+        }
+    }
+}
