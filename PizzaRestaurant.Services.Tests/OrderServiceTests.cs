bbbf99c46f18d8487c6f655830e409a23064a6e4
Ivaylo Petkov <ivi_petkow@abv.bg>
2023-08-10 19:37:05 +0300

    Added ProductService tests

--- PizzaRestaurant.Services.Tests/OrderServiceTests.cs
+++ PizzaRestaurant.Services.Tests/OrderServiceTests.cs
@@ -26,6 +26,7 @@
             this.dbContext.Database.EnsureCreated();
             SeedDatabase(this.dbContext);
 
+            this.cartService = new CartService(this.dbContext);
             this.orderService = new OrderService(this.dbContext, this.cartService);
         }
 
@@ -37,32 +38,38 @@
 
             await orderService.AddOrderAsync(userId);
 
-            var order = await dbContext.Orders.FirstOrDefaultAsync();
+            var order = await dbContext
+                .Orders
+                .FirstOrDefaultAsync();
             Assert.NotNull(order);
             Assert.AreEqual(14.99M, order.Price);
         }
 
         [Test]
-        public async Task EmptyCartAsync_ShouldRemoveCartsForUser()
+        public async Task EmptyCartAsyncShouldRemoveCartsForUser()
         {
             var userId = "59df8a72-7c6e-4c32-9b1e-eb1d07d17f79"; 
             await cartService.AddPizzaToCartAsync(1, 14.99M, userId); 
 
             await orderService.EmptyCartAsync(userId);
 
-            var cartsCount = await dbContext.Carts.CountAsync(c => c.UserId == Guid.Parse(userId));
+            var cartsCount = await dbContext
+                .Carts
+                .CountAsync(c => c.UserId == Guid.Parse(userId));
             Assert.AreEqual(0, cartsCount);
         }
 
         [Test]
-        public async Task RemoveCartPizzasAsync_ShouldRemoveCartPizzasForUser()
+        public async Task RemoveCartPizzasAsyncShouldRemoveCartPizzasForUser()
         {
             var userId = "59df8a72-7c6e-4c32-9b1e-eb1d07d17f79"; 
             await cartService.AddPizzaToCartAsync(1, 14.99M, userId); 
 
             await orderService.RemoveCartPizzasAsync(userId);
 
-            var cartPizzasCount = await dbContext.CartsPizzas.CountAsync(cp => cp.UserId == Guid.Parse(userId));
+            var cartPizzasCount = await dbContext
+                .CartsPizzas
+                .CountAsync(cp => cp.UserId == Guid.Parse(userId));
             Assert.AreEqual(0, cartPizzasCount);
         }
     }
